b9502427b731bbfb6ae56b03f9ace45f68968598
Vinisha Reddy <vinishareddym@gmail.com>
2025-12-08 10:46:28 +0000

    fest(views): update index

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+"""
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index.")
-
+"""
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
@@ -16,3 +17,12 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
